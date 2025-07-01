b42fd1c468f025e9d50cac8dce16150fafa1c266
miawoolfson <78734051+miawoolfson@users.noreply.github.com>
2025-07-01 20:50:28 +0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    allowedHosts: 'all', 
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
